87d8ff9040ba76b8be4c67a378df106ae92d9fd2
Mainul001 <luniam.mi@gmail.com>
2015-03-06 12:43:10 +0600

    Initial Commit

---
+++ CircleCalculation/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCalculation
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double getDiameter()
+        {
+            return Radius*2;
+        }
+
+        public double getArea()
+        {
+            return Math.PI*Math.Pow(Radius, 2);
+        }
+
+        public double getPerimeter()
+        {
+            return 2*Math.PI*Radius;
+        }
+    }
+}
---
+++ CircleCalculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            Circle aCircle = new Circle(radius);
+
+            Console.WriteLine("The diameter is " + aCircle.getDiameter());
+            Console.WriteLine("Area " + aCircle.getArea());
+            Console.WriteLine("Perimeter(Circumference) " + aCircle.getPerimeter());
+
+            Console.ReadLine();
+        }
+    }
+}
